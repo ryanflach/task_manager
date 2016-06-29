649f354047ee17cd93107ee1bd46afed54e2af78
Ryan Flach <ryanflach@users.noreply.github.com>
2016-06-29 10:09:21 -0600

    Initial setup of environment variable and TestHelpers module

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,16 @@
+# Set up environment variable for test database
+ENV['RACK_ENV'] ||= 'test'
+
 require File.expand_path("../../config/environment", __FILE__)
 
 require 'minitest/autorun'
 require 'minitest/pride'
+
+module TestHelpers
+
+  def task_manager
+    database = YAML::Store.new('db/task_manager_test')
+    @database ||= TaskManager.new(database)
+  end
+
+end
